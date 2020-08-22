00d74c5099de9beb746e3ee5a613865fef9eb141
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-08-22 17:26:34 +0900

    add start_from_existing

--- switch2f9j_tpwe/roulette.py
+++ switch2f9j_tpwe/roulette.py
@@ -7,7 +7,7 @@ from random import randint, choices
 
 POPULATIONS_CLASSES = 6
 POPULATIONS_NUM = 12
-GENERATION = 1000
+GENERATION = 1200
 
 
 if __name__ == '__main__':
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         print('    num', cls_num)
 
         # load json
-        with open('./5th/populations' + str(cls_num) + '/population.json') as f:
+        with open('./6th/populations' + str(cls_num) + '/population.json') as f:
             setting = json.load(f)
 
         # select population by roulette
